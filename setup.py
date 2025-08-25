1e94d857de67b99cb4d580eb1dc23c9f5c7f2b6f
Maxime-pixel <maxime.outteryck@qperfect.io>
2025-08-25 17:14:12 +0200

    chore: test qleo

---
+++ setup.py
@@ -0,0 +1,24 @@
+import sys
+import platform
+from setuptools import setup, find_packages
+
+data_files_to_install = []
+
+python_ver = platform.python_version().rsplit(".", 1)[0]
+
+if sys.platform == 'linux':
+    # Code to run only on Linux
+    print("Building for Linux...")
+    data_files_to_install.append((f"lib/python{python_ver}/site-packages/targets",
+                                  ["libs/cudaqleo/python/targets/Qleo.yml"]))
+    data_files_to_install.append((f"lib/python{python_ver}/site-packages/lib",
+                                  ["libs/cudaqleo/python/lib/libnvqir-Qleo.so"]))
+
+setup(
+    name="qleo",
+    version="0.1",
+    packages=["qleo"],
+    package_dir={"qleo": "src/qleo"},
+    include_package_data=True,
+    data_files=data_files_to_install,
+)
