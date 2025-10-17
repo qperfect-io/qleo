76e1af95640eb2c13889e8ea2f7bb8a72b735a87
Maxime-pixel <maxime.outteryck@qperfect.io>
2025-10-17 15:55:22 +0200

    fix: update version and add Gpu to shared library copied

--- setup.py
+++ setup.py
@@ -13,6 +13,10 @@ if sys.platform == 'linux':
                                   ["src/targets/Qleo.yml"]))
     data_files_to_install.append((f"lib/python{python_ver}/site-packages/lib",
                                   ["src/lib/libnvqir-Qleo.so"]))
+    data_files_to_install.append((f"lib/python{python_ver}/site-packages/targets",
+                                  ["src/targets/QleoGpu.yml"]))
+    data_files_to_install.append((f"lib/python{python_ver}/site-packages/lib",
+                                  ["src/lib/libnvqir-QleoGpu.so"]))
 
 setup(
     name="qleo",
